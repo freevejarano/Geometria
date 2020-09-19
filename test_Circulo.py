baf4604344c533814bca53cbc470107d72590292
alejandro <luis.vejarano01@correo.usa.edu.co>
2020-09-19 15:33:02 -0500

    Agrego Unitest en Circulo

--- test_Circulo.py
+++ test_Circulo.py
@@ -3,14 +3,17 @@ from unittest import TestCase
 from Circulo import Circulo
 from Punto import Punto
 
+a = Punto(0, 0)
+b = Punto(0, 4)
+c = [a, b]
+circulo = Circulo(c)
 
 class Test(TestCase):
-    def test_circulo(self):
-        a = Punto(0, 0)
-        b = Punto(0, 4)
-        c = [a, b]
-        circulo = Circulo(c)
-        w = Punto(5, 5)
+
+    def test_EnElcirculoTrue(self):
         z = Punto(2,2)
         self.assertTrue(circulo.determinarEnElCirculo(z),"Si")
+
+    def test_EnElCirculoFalse(self):
+        w = Punto(5, 5)
         self.assertFalse(circulo.determinarEnElCirculo(w),"Si")
